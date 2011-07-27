c54429901fa462a2f31170a27e7b01c977a03a68
Chandler Kent <ckentster@gmail.com>
2011-07-27 15:09:00 -0500

    Made helpers.buildPageContext accept a variable list of arguments. First argument is a request object and all others are JS objects which will be combined to build page context.

--- server/helpers.js
+++ server/helpers.js
@@ -3,13 +3,26 @@ var view_helpers = require("./view_helpers");
 var CONFIG = require("./config");
 
 module.exports = {
-  buildPageContext: function(req, options) {
-    var ctx = options || {};
+  // builds an object to be passed to the render function
+  buildPageContext: function(/* HTTPRequest, obj1, obj2, ...*/) {
+    var args = Array.prototype.slice.call(arguments);
+    var req = args[0];
+    var ctx = {};
     ctx.site = CONFIG.SITE;
     ctx.path = URL.parse(req.url).pathname;
-    ctx.page = ctx.page || {};
     ctx.helpers = view_helpers;
-
+    
+    for (var i = 1; i < args.length; i++) {
+      var arg = args[i];
+      for (var prop in arg) {
+        if (arg.hasOwnProperty(prop)) {
+          ctx[prop] = arg[prop];
+        }
+      }
+    }
+    
+    ctx.page = ctx.page || {};
+    
     return { context: ctx };
   },
   
